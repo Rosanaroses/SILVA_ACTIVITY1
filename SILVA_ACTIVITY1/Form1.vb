8bcedf2b0ac838524fff4aa619bd2279f3f2ed87
STC <STC@DESKTOP-CGLT37F>
2024-02-08 14:03:57 +0800

    Add project files.

---
+++ SILVA_ACTIVITY1/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+
+
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Integer
+        Dim b As Integer
+        a = txtFirstNum.Text
+        b = txtSecondNum.Text
+        lblResult.Text = a + b
+    End Sub
+End Class
+
